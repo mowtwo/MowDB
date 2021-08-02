241c54d06f80b4eaf75e7abc293011554f1033c1
chenwc <1046099511@qq.com>
2021-08-02 17:57:58 +0800

    fix 修改测试用例

--- test/DbTable.test.ts
+++ test/DbTable.test.ts
@@ -1,5 +1,6 @@
 import DbTable from "../src/DbTable";
 import { Eq, Ge, Le, Like, Lt, OprPipe } from "../src/FindOpr";
+import * as dayjs from "dayjs"
 
 class Entity {
   constructor(
@@ -11,18 +12,26 @@ class Entity {
 
 const table = new DbTable<Entity>('user')
 
-table.add(new Entity('mowtwo', 10, new Date('1998/06/09')))
-  .add(new Entity('mowtwo', 20, new Date('1998/06/09')))
-  .add(new Entity('mow', 30, new Date('1998/06/09')))
-  .add(new Entity('mowOne', 40, new Date('1998/06/09')))
+table.add(new Entity('mowtwo', 10, new Date('1998/06/01')))
+  .add(new Entity('mowtwo', 20, new Date('1998/06/02')))
+  .add(new Entity('mow', 30, new Date('1998/06/03')))
+  .add(new Entity('mowOne', 40, new Date('1998/06/04')))
+  .add(new Entity('thismow', 50, new Date('1998/06/05')))
+  .add(new Entity('thismow', 50, new Date('1998/06/06')))
+  .add(new Entity('thismow', 50, new Date('1998/06/07')))
+  .add(new Entity('thismow', 50, new Date('1998/06/08')))
   .add(new Entity('thismow', 50, new Date('1998/06/09')))
-  .add(new Entity('thismow', 50, new Date('1998/06/09')))
-  .add(new Entity('thismow', 50, new Date('1998/06/09')))
-  .add(new Entity('thismow', 50, new Date('1998/06/09')))
-  .add(new Entity('thismow', 50, new Date('1998/06/09')))
-  .add(new Entity('thismow', 50, new Date('1998/06/09')))
-  .add(new Entity('thismow', 50, new Date('1998/06/09')))
+  .add(new Entity('thismow', 50, new Date('1998/06/10')))
+  .add(new Entity('thismow', 50, new Date('1998/06/11')))
 console.log(table.find({
-  take: 3,
-  skip: 2
-}).toObject())
+  birth: OprPipe(
+    Ge(new Date('1998/06/03')),
+    Lt(new Date('1998/06/09'))
+  ),
+  groupBy: ['birth', 'age']
+}).toArray().map(item => {
+  return {
+    ...item,
+    birth: dayjs(item.birth).format('YYYY-MM-DD')
+  }
+}))
